289030630807e18ddede353fe0446d9e0a4a452b
abkhale <abkhale.17@gmail.com>
2020-09-09 11:43:30 +0530

    #1,#2 create todos using factories

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
-console.log("This is a test")
+const addTodoList = (() => {
+	var todosList = [], defaultProject = []
+
+	const addTask = (todo) => {
+		todosList.push(todo)
+	}
+	return {addTask}
+})()
+
+const todos = (title, description, dueDate, priority, notes) => {
+	let todo = {}
+	todo.title = title;
+	todo.description = description;
+	todo.dueDate = dueDate;
+	todo.priority = priority;
+	todo.notes = notes;
+
+	return todo
+
+}
+
+var newTask = todos('a','b','c','d','e')
+addTodoList.addTask(newTask)
+
+var newTask2 = todos('m','b','c','d','e')
+addTodoList.addTask(newTask2)
